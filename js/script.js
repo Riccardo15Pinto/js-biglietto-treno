c6967022d8e7d36b1a7f33cb0a11ae7f217f6913
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-05-26 17:17:53 +0200

    add discounttotal1 and checkit

--- js/script.js
+++ js/script.js
@@ -13,10 +13,18 @@ const distanceUser = parseInt( prompt('Quanti chilometri vuoi percorrere?'))
 // check distanceUser
 console.log(distanceUser);
 // add totalprice
-const price = (distanceUser * 0.21).toFixed(2);
+let price = (distanceUser * 0.21).toFixed(2);
 // check price
 console.log(price);
-// add discount
-const discount20 = ((price * 20) / 100);
-const discount40 = ((price * 40) / 100);
-
+// add discount20
+let discount20 = ((20 / 100) * price).toFixed(2);
+console.log(discount20)
+// add discount total
+let discountTotal = price - discount20;
+// check discountTotal
+console.log(discountTotal);
+// add discount40
+let discount40 = ((40 / 100) * price).toFixed(2);
+// check discountTotal
+let discountTotal1 = price - discount40;
+console.log(discountTotal1);
