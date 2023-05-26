afe7dfc927cf1bca82840fe61685e3ebc23d0df9
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-05-26 16:24:54 +0200

    add and check distanceUser

--- js/script.js
+++ js/script.js
@@ -4,6 +4,11 @@ console.log("ok js");
 console.log(document.getElementById('age-user'));
 console.log(document.getElementById('distance'));
 console.log(document.getElementById('price'));
-
-//const ageUser = parseInt( prompt('Quanti anni hai?'))
-//console.log(ageUser);
+// add prompt ageUser
+const ageUser = parseInt( prompt('Quanti anni hai?'))
+// check ageUser
+console.log(ageUser);
+// add prompt distanceUser
+const distanceUser = parseInt( prompt('Quanti chilometri vuoi percorrere?'))
+// check distanceUser
+console.log(distanceUser);
