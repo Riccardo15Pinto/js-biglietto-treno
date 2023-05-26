73589ca2c7f56be92a8b1bb583a113998eeb67e7
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-05-26 17:46:30 +0200

    add variable and add text in the p element

--- js/script.js
+++ js/script.js
@@ -18,13 +18,18 @@ let price = (distanceUser * 0.21).toFixed(2);
 console.log(price);
 // add discount20
 let discount20 = ((20 / 100) * price).toFixed(2);
-console.log(discount20)
-// add discount total
-let discountTotal = price - discount20;
-// check discountTotal
-console.log(discountTotal);
 // add discount40
 let discount40 = ((40 / 100) * price).toFixed(2);
-// check discountTotal
-let discountTotal1 = price - discount40;
-console.log(discountTotal1);
+
+let discountTotal = price ;
+
+if(ageUser < 18 ){
+    discountTotal = (price - discount20).toFixed(2);
+}else if (ageUser > 65){
+    discountTotal = (price - discount40).toFixed(2);
+}
+
+console.log(discountTotal)
+document.getElementById("age-user").innerText = "La tua età è:" +" "+ ageUser + "anni";
+document.getElementById("distance").innerText = "Il tragitto che vuoi percorrere è di:" +" "+ distanceUser + "Km";
+document.getElementById("price").innerText = "Il costo del tuo biglietto è di:" + " " + discountTotal + "€";
