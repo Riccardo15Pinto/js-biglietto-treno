7fe1207260740fca3feb07b44b0f315b90945fe7
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-05-26 16:21:50 +0200

    check elements dom

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
 // check connect javascript
 console.log("ok js");
+// check DOM element
+console.log(document.getElementById('age-user'));
+console.log(document.getElementById('distance'));
+console.log(document.getElementById('price'));
+
+//const ageUser = parseInt( prompt('Quanti anni hai?'))
+//console.log(ageUser);
